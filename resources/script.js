2a22c4f110ce95d537420cfdf37c01122b4041a4
Jen <evunti@gmail.com>
2024-10-17 10:13:25 -0400

    fixed the getUserChoice var and computerChoice var

--- resources/script.js
+++ resources/script.js
@@ -1,17 +1,44 @@
 function getComputerChoice() {
-    const randomChoice = Math.random();
-    if (randomChoice < 0.33) {
-        return "rock";
-    } else if (randomChoice < 0.66) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
+  const randomChoice = Math.random();
+  if (randomChoice < 0.33) {
+    return "rock";
+  } else if (randomChoice < 0.66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
 
-let userChoice = prompt("Choose rock, paper, or scissors");
+let getUserChoice = prompt("Choose rock, paper, or scissors");
+let computerChoice = getComputerChoice();
+if (
+  getUserChoice === "rock" ||
+  getUserChoice === "paper" ||
+  getUserChoice === "scissors"
+) {
+  // generate a new computer choice
+  console.log(
+    "You chose: " + getUserChoice + ". " + "Computer chose: " + computerChoice
+  );
+}
 
-if (userChoice === "rock" || userChoice === "paper" || userChoice === "scissors") {
-     console.log("You chose: " + userChoice + "." + " " + "Computer chose: " + getComputerChoice());;}
-    
-   
+let humanScore = 0;
+let computerScore = 0;
+
+// define what beats what
+function playRound(getUserChoice, computerChoice) {
+  if (getUserChoice === computerChoice) {
+    return "It's a tie!";
+  } else if (
+    (getUserChoice === "rock" && computerChoice === "scissors") ||
+    (getUserChoice === "scissors" && computerChoice === "paper") ||
+    (getUserChoice === "paper" && computerChoice === "rock")
+  ) {
+    return "You win!";
+  } else {
+    return "You Lose";
+  }
+}
+console.log(playRound(getUserChoice, computerChoice));
+
+// if user won print uesr won +1 humanScore
