d31c87ea2feb4c32746f6a6f6b14d355f67e2a1d
Jen <evunti@gmail.com>
2024-10-23 09:56:43 -0400

    added html and css styling, commented out some js, also added instuctions to readme

--- resources/script.js
+++ resources/script.js
@@ -31,27 +31,24 @@ function playRound(getUserChoice, computerChoice) {
 }
 
 function playGame() {
-  for (let i = 0; i < 5; i++) {
-    let getUserChoice = prompt("Choose rock, paper, or scissors");
-    getUserChoice = getUserChoice.toLowerCase();
-    if (
-      getUserChoice === "rock" ||
-      getUserChoice === "paper" ||
-      getUserChoice === "scissors"
-    ) {
-      let computerChoice = getComputerChoice();
-      console.log(
-        "You chose: " +
-          getUserChoice +
-          ". " +
-          "Computer chose: " +
-          computerChoice
-      );
-      console.log(playRound(getUserChoice, computerChoice));
-    } else {
-      console.log("Invalid choice. Please choose rock, paper, or scissors.");
-      i--;
-    }
+  // for (let i = 0; i < 5; i++)
+  // {
+  // let getUserChoice = prompt("Choose rock, paper, or scissors");
+  // getUserChoice = getUserChoice.toLowerCase();
+  if (
+    getUserChoice === "rock" ||
+    getUserChoice === "paper" ||
+    getUserChoice === "scissors"
+  ) {
+    let computerChoice = getComputerChoice();
+    console.log(
+      "You chose: " + getUserChoice + ". " + "Computer chose: " + computerChoice
+    );
+    console.log(playRound(getUserChoice, computerChoice));
+  } else {
+    console.log("Invalid choice. Please choose rock, paper, or scissors.");
+    i--;
+    // }
   }
   console.log(`Final Score: You - ${humanScore}, Computer - ${computerScore}`);
 }
